815f09ac0be88ca3c18440723dcc5089814b32f4
SooweKun <Apextoplega@mail.ru>
2024-05-27 22:00:03 +0500

    +

--- src/components/forms/Form.jsx
+++ src/components/forms/Form.jsx
@@ -1,37 +1,41 @@
-/* eslint-disable react/react-in-jsx-scope */
-import {useState} from 'react';
+
+/* eslint-disable react/no-unescaped-entities */
+import React from 'react';
+import {useForm} from 'react-hook-form';
 import styles from './Form.module.css';
 
 export const Form = () => {
-	const [stateInput, setStateInput] = useState({
-
-	});
-
-	const btnClick = () => {
-		console.log(stateInput);
-	};
-
+	const {register, handleSubmit} = useForm();
 	return (
-
 		<div className={styles.form}>
 			<div className={styles.back}>
 				<button>{'<'}</button>
 				<h1>Sing in</h1>
 			</div>
 
-			<div className={styles.fon}>
-				<div className={styles.inpt}>
-					<input type='text' placeholder='Name' onChange={e => setStateInput({...stateInput, name: e.target.value})} />
-					<input type='text' placeholder='Mail' onChange={e => setStateInput({...stateInput, mail: e.target.value})} />
-					<input type='text' placeholder='Password' onChange={e => setStateInput({...stateInput, password: e.target.value})} />
-					<a href='#'>Forgot password?</a>
-					<button onClick={btnClick}>Sing in</button>
-				</div>
+			<div className={styles.fon}></div>
+			<div className={styles.inpt}>
+				<input type='text' placeholder='Name' />
+				<input type='text' placeholder='Mail' />
+				<input type='text' placeholder='Password' />
+				<a href='#'>Forgot password?</a>
+				<button>Sing in</button>
 			</div>
+
+			{/* может быть компонентом содержащим пропсы children являющиеся media или же просто массивом url с картинками */}
 			<div className={styles.atribute}>
-				<hr />
-				<p>OR</p>
-				<hr />
+				<div className={styles.colontitul}>
+					<div className={styles.line}></div>
+					<p>OR</p>
+					<div className={styles.line}></div>
+				</div>
+
+				<div className={styles.media}>
+					<img className={styles.onli} src='src/assets/icons/Mask group.svg' alt='' />
+					<img className={styles.apple} src='src/assets/icons/apple.svg' alt='' />
+					<img className={styles.google} src='src/assets/icons/Vector.svg' alt='' />
+				</div>
+				<a href='#'><span>Don't have an account?</span> Sing up</a>
 			</div>
 		</div>
 	);
